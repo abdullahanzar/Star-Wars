de9149a66bc74b4068623af577606e7d8c5ee794
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-12-23 10:11:19 +0530

    Major Functionalities Complete

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,11 +3,13 @@ import "./Categories.css";
 import Folder from "../assets/Folder.png";
 import ActionButton from "../assets/Action.png";
 import CategoriesContext from "./CategoriesContext";
+import StarWarsContext from "../StarWarsContext";
 
 export default function Categories(props) {
-  const {setSelected} = useContext(CategoriesContext);
+  const {setSelected, selected} = useContext(CategoriesContext);
+  const {category} = useContext(StarWarsContext);
   return (
-    <div className="category" onClick={()=>setSelected(props.name.toLowerCase())}>
+    <div className={props.name.toLowerCase()==selected ? "category category-selected" : "category"} onClick={()=>setSelected(props.name.toLowerCase())}>
       <img src={Folder} alt="" />
       <p>{props.name}</p>
       <img src={ActionButton} alt="" />
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@ import "./Slider.css";
 import StarWarsContext from "../StarWarsContext";
 
 export default function Slider() {
-  const { slider, setSlider } = useContext(StarWarsContext);
+  const { slider, setSlider, category } = useContext(StarWarsContext);
   const [keys, setKeys] = useState([]);
   useEffect(() => {
     console.log(slider)
@@ -13,31 +13,25 @@ export default function Slider() {
   return (
     <div className="overlay" onClick={() => setSlider("")}>
       <div className="slider">
-        <p>Details</p>
-        {keys.includes("title") && (
-          <div className="image">
-            <p>Image</p>
-            <img src={slider[1]} alt="" />
-          </div>
-        )}
-        {(keys.includes("name") || keys.includes("title")) && <div className="title">
-          <p>Title</p>
-          <p>{slider[0].title || slider[0].name}</p>
-        </div>}
-        <div className="genre">
-          <p>{keys[1]}</p>
-          <p>{slider["rotation_period"]}</p>
-        </div>
-        <div>
+          <p className="slider_title">{capitalizeFirstLetter(category)} Details</p>
+          <div className="divider"></div>
+          <div className="slider_data">
           {
             keys.map((item)=>{
               return (
-                <p>{item} : {slider[0][item]}</p>
+                <div className="node">
+                <p>{capitalizeFirstLetter(item)}</p>
+                <p className="data_style">{slider[0][item]}</p>
+                </div>
               )
             })
           }
-        </div>
+          </div>
       </div>
     </div>
   );
+}
+
+function capitalizeFirstLetter(str) {
+  return str[0].toUpperCase() + str.slice(1)
 }
